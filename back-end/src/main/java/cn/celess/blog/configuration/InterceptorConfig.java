cfef34c221630cc9f7f22926c69a51cadf4e3d47
小海 <zh56462271@qq.com>
2019-10-18 16:41:20 +0800

    详细记录api的访问情况

--- back-end/src/main/java/cn/celess/blog/configuration/InterceptorConfig.java
+++ back-end/src/main/java/cn/celess/blog/configuration/InterceptorConfig.java
@@ -1,6 +1,10 @@
 package cn.celess.blog.configuration;
 
 import cn.celess.blog.configuration.filter.MultipleSubmitFilter;
+import cn.celess.blog.configuration.filter.VisitorRecord;
+import cn.celess.blog.configuration.listener.SessionListener;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,5 +19,14 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MultipleSubmitFilter()).addPathPatterns("/*");
+        registry.addInterceptor(new VisitorRecord()).addPathPatterns("/*");
+    }
+
+    // session listener register bean
+    @Bean
+    public ServletListenerRegistrationBean<SessionListener> servletListenerRegistrationBean() {
+        ServletListenerRegistrationBean<SessionListener> slrBean = new ServletListenerRegistrationBean<SessionListener>();
+        slrBean.setListener(new SessionListener());
+        return slrBean;
     }
 }
