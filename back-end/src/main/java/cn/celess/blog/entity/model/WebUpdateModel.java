3d819e620a5428d8088423f4713fbe93ba40ed09
小海 <zh56462271@qq.com>
2019-11-20 14:33:06 +0800

    WebUpdate接口的测试

--- back-end/src/main/java/cn/celess/blog/entity/model/WebUpdateModel.java
+++ back-end/src/main/java/cn/celess/blog/entity/model/WebUpdateModel.java
@@ -1,12 +1,14 @@
 package cn.celess.blog.entity.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author : xiaohai
  * @date : 2019/05/12 11:32
  */
 @Data
+@NoArgsConstructor
 public class WebUpdateModel {
     private long id;
 
